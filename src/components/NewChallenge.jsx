67784462d4fda3ee621b24f1c47aeed55bd7ec5f
arcade15xyz <arcade15xyz@gmail.com>
2025-08-12 13:04:17 +0530

    Animating Colors and working with keyframes

--- src/components/NewChallenge.jsx
+++ src/components/NewChallenge.jsx
@@ -82,10 +82,9 @@ export default function NewChallenge({ onDone }) {
                         <motion.li
                             variants={{
                                 hidden: { opacity: 0, scale: 0.5 },
-                                visible: { opacity: 1, scale: 1 },
+                                visible: { opacity: 1, scale: [0.8, 1.3, 1] },
                             }}
                             exit={{ opacity: 1, scale: 1 }}
-                            transition={{ type: "spring" }}
                             key={image.alt}
                             onClick={() => handleSelectImage(image)}
                             className={
